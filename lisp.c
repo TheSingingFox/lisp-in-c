20c3ddd3e3fd55d839e45c38c968c7e05872e0fd
TheSingingFox <singingfox123@gmail.com>
2020-08-21 23:18:31 +0200

    foxlisp 0.0.0.0.2

--- lisp.c
+++ lisp.c
@@ -1,2 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+
+/* Declare a buffer for user input of size 2048 */
+static char input[2048];
+
+int main(int argc, char** argv) {
+
+  /* Print Version and Exit Information */
+  puts("foxlisp Version 0.0.0.0.2");
+  puts("Type (exit) to exit\n");
+
+  /* In a never ending loop */
+  for(int stay = 1; stay;) {
+
+    /* Output our prompt */
+    fputs("foxlisp> ", stdout);
+
+    /* Read a line of user input of maximum size 2048 */
+    fgets(input, 2048, stdin);
+
+    if(!strncmp(input, "(exit)", 6)) {
+      stay = 0;
+    } else {
+      /* Echo input back to user */
+      printf("%s", input);
+    }
+  }
+
+  return 0;
+}
