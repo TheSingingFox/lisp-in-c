7a6d1f9d5e117faed54182a649eb264a632782ac
TheSingingFox <singingfox123@gmail.com>
2020-08-21 23:33:02 +0200

    foxlisp 0.0.0.0.3

--- lisp.c
+++ lisp.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-/* Declare a buffer for user input of size 2048 */
-static char input[2048];
+#include <editline/readline.h>
 
 int main(int argc, char** argv) {
 
   /* Print Version and Exit Information */
-  puts("foxlisp Version 0.0.0.0.2");
+  puts("foxlisp Version 0.0.0.0.3");
   puts("Type (exit) to exit\n");
 
   /* In a never ending loop */
   for(int stay = 1; stay;) {
 
-    /* Output our prompt */
-    fputs("foxlisp> ", stdout);
+    /* Output our prompt and get input */
+    char *input = readline("foxlisp> ");
 
-    /* Read a line of user input of maximum size 2048 */
-    fgets(input, 2048, stdin);
+    /* Add input to history */
+    add_history(input);
 
+    /* Create an exit condition */
     if(!strncmp(input, "(exit)", 6)) {
       stay = 0;
     } else {
       /* Echo input back to user */
-      printf("%s", input);
+      printf("%s\n", input);
     }
+
+    /* Free retrieved input */
+    free(input);
   }
 
   return 0;
